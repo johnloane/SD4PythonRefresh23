7d6f8680a1ae4f4487f4c001504838fb678b9345
John Loane <john.loane@dkit.ie>
2022-10-10 10:48:32 +0100

    Added video for lane finding
    Will look at the video one frame at a time

--- lanes.py
+++ lanes.py
@@ -1,10 +1,9 @@
 import cv2
 import numpy as np
 
-
 def region_of_interest(image):
     height = image.shape[0]
-    polygons = np.array([[(100, 1000), (1350, height), (1050, 600)]])
+    polygons = np.array([[(0, 700), (500, 375), (960, 1000)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask)
@@ -23,48 +22,52 @@ def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            if line is not None:
+                x1, y1, x2, y2 = line.reshape(4)
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     return line_image
-
 
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
+    if lines is None:
+        return None
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, y1), (x2, y2), 1)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
         if slope < 0:
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
-    left_fit_average = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    left_line = make_coordinates(image, left_fit_average)
-    right_line = make_coordinates(image, right_fit_average)
-    return np.array([left_line, right_line])
+    if len(left_fit) and len(right_fit):
+        left_fit_average = np.average(left_fit, axis=0)
+        right_fit_average = np.average(right_fit, axis=0)
+        left_line = make_coordinates(image, left_fit_average)
+        right_line = make_coordinates(image, right_fit_average)
+        return np.array([left_line, right_line])
 
 
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
-    y1 = image.shape[0]
-    y2 = int(y1 * (2/5))
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return np.array([x1, y1, x2, y2])
+    if line_parameters is not None and abs(slope) > 0.001:
+        y1 = image.shape[1]
+        y2 = int(y1 * (9/20))
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept)/slope)
+        return np.array([x1, y1, x2, y2])
 
 
 
-image = cv2.imread('country-road.jpg')
+image = cv2.imread('IMG_2095.jpg')
+image = cv2.resize(image, (960, 1280))
 lane_image = np.copy(image)
 canny_image = canny(lane_image)
 cropped_image = region_of_interest(canny_image)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=10)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=100)
 averaged_lines = average_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-
 cv2.imshow("Window name", combo_image)
 cv2.waitKey(0)
