007f1de774e03cfb6e5ecec079d22adeaa057bb2
John Loane <john.loane@dkit.ie>
2022-10-17 10:58:09 +0100

    Worked out probabilities for all points
    Need to add cross entropy to work out the error and gradient descent
    to change the weights to move in that direction to minimize errors

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n_pts = 10
+
+def draw(x1, x2):
+    ln = plt.plot(x1, x2)
+
+
+def sigmoid(score):
+    return 1/(1+np.exp(-score))
+
+n_pts = 100
 np.random.seed(0)
 bias = np.ones(n_pts)
 
@@ -12,9 +20,23 @@ top_region = np.array([random_x1_values, random_x2_values, bias]).T
 bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(6, 2, n_pts), bias]).T
 
 all_points = np.vstack((top_region, bottom_region))
-print(all_points)
+w1 = -0.2
+w2 = -0.35
+b = 3.5
+
+line_parameters = np.matrix([w1, w2, b]).T
+
+x1 = np.array([bottom_region[:, 0].min(), top_region[:, 0].max()])
+#w1x1+w2x2+b=0
+x2 = -b/w2 + x1*(-w1/w2)
+
+linear_combination = all_points * line_parameters
+probabilities = sigmoid(linear_combination)
+print(probabilities)
+
 
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
 ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
+draw(x1, x2)
 plt.show()
