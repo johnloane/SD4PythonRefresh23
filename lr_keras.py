4083628af6d096a2b85cc79f561fc79062fc3a93
John Loane <john.loane@dkit.ie>
2022-10-25 20:16:33 +0100

    Drawing boundary of Keras Model
    Also used the model to predict the probability for a given point

--- lr_keras.py
+++ lr_keras.py
@@ -5,6 +5,20 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
+
+def plot_decision_boundary(X, Y, model):
+    x_span = np.linspace(min(X[:, 0] -1), max(X[:, 0] + 1))
+    y_span = np.linspace(min(X[:, 1] -1), max(X[:, 1] + 1))
+    xx, yy = np.meshgrid(x_span, y_span)
+    xx_, yy_ = xx.ravel(), yy.ravel()
+    grid = np.c_[xx_, yy_]
+    pred_func = model.predict(grid)
+    z = pred_func.reshape(xx.shape)
+    plt.contourf(xx, yy, z)
+
+
+
+
 n_pts = 500
 np.random.seed(0)
 Xa = np.array([np.random.normal(13, 2, n_pts), np.random.normal(12, 2, n_pts)]).T
@@ -29,10 +43,25 @@ model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
 # X is the data, Y are the labels
 h = model.fit(x=X, y=Y, verbose=1, batch_size = 50, epochs=10, shuffle='true')
 
-plt.plot(h.history['loss'])
-plt.title('Loss')
-plt.xlabel('epoch')
-plt.legend(['Loss'])
+plot_decision_boundary(X, Y, model)
+
+plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
+plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
+
+x = 7.5
+y = 2.5
+
+point = np.array([[x, y]])
+prediction = model.predict(point)
+plt.plot([x], [y], marker='o', markersize=10, color='red')
+print("Prediction is ", prediction)
+
+
+
+#plt.plot(h.history['loss'])
+#plt.title('Loss')
+#plt.xlabel('epoch')
+#plt.legend(['Loss'])
 
 # plt.scatter(X[:n_pts, 0], X[:n_pts, 1], color='r')
 # plt.scatter(X[n_pts:, 0], X[n_pts:, 1], color='b')
