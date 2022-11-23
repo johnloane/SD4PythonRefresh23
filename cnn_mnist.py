22ebabf255d98f913671bd9a62e29812f2f4dc9c
John Loane <john.loane@dkit.ie>
2022-11-23 09:06:48 +0000

    Convolutional Neural Network for mnist
    dnn and cnn for mnist data

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -5,6 +5,9 @@ from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils.np_utils import to_categorical
 import random
 import requests
@@ -12,13 +15,18 @@ from PIL import Image
 import cv2
 
 
-def create_model():
+def leNet_model():
     model = Sequential()
-    model.add(Dense(10, input_dim=num_pixels, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(15, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
     model.compile(Adam(learning_rate=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
 
 
 np.random.seed(0)
@@ -56,48 +64,13 @@ y_test = to_categorical(y_test, 10)
 X_train = X_train/255
 X_test = X_test/255
 
-num_pixels = 784
-X_train = X_train.reshape(X_train.shape[0], num_pixels)
-X_test = X_test.reshape(X_test.shape[0], num_pixels)
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 
-model = create_model()
+model = leNet_model()
 print(model.summary())
-history = model.fit(X_train, y_train, validation_split=0.1, epochs=28, batch_size=200, verbose=1, shuffle=1)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.legend(['loss', 'val_loss'])
-plt.title('Loss')
-plt.xlabel('epochs')
-plt.show()
 
 score = model.evaluate(X_test, y_test, verbose=0)
-print("Test score: ", score[0])
-print("Test accuracy: ", score[1])
-
-url = 'https://colah.github.io/posts/2014-10-Visualizing-MNIST/img/mnist_pca/MNIST-p1815-4.png'
-response = requests.get(url, stream=True)
-img = Image.open(response.raw)
-#plt.imshow(img)
-#plt.show()
-
-img_array = np.asarray(img)
-resized = cv2.resize(img_array, (28, 28))
-#print(resized.shape)
-grayscale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-#print(grayscale.shape)
-#plt.imshow(grayscale, cmap=plt.get_cmap('gray'))
-#plt.show()
-image = cv2.bitwise_not(grayscale)
-#plt.imshow(image, cmap=plt.get_cmap('gray'))
-#plt.show()
-image = image/255
-image = image.reshape(1, 784)
-prediction = np.argmax(model.predict(image), axis=1)
-print("Predicted digit: ", str(prediction))
-
-
-
-
-
-
+print('Test accuracy: ', score[1])
+# LeNet Input->Conv1->Pool1->Conv2->Pool2->Fully_Connected->Output
 
